edf91fbfa41b4afe356be84619437fdfe2177b94
hb4 <hb4@hb4-PC>
2015-12-08 11:05:51 +0900

    123

--- hnb/src/main/java/com/hnb/main/MainController.java
+++ hnb/src/main/java/com/hnb/main/MainController.java
@@ -11,20 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class MainController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("메인 컨트롤러 home 메소드 진입!! {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
---
+++ hnb/src/main/java/com/hnb/member/MemberController.java
@@ -0,0 +1,10 @@
+package com.hnb.member;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hnb.main.MainController;
+
+public class MemberController {
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+}
